0f22765c84c2307c169a14c9887635b109f1a372
Ying Chan <89020187+ychan2020@users.noreply.github.com>
2022-05-29 15:18:28 +0800

    Updated without localhost

--- iot_publish_doorlock.py
+++ iot_publish_doorlock.py
@@ -5,12 +5,16 @@ iot_publish_doorlock.py without auth and tls
 import sys
 from paho.mqtt import publish
 
-if len(sys.argv) == 1:
+HOST = ""
+REQUEST = ""
+
+if len(sys.argv) == 3:
+    HOST = str(sys.argv[1])
+    REQUEST = str(sys.argv[2])
+else:
     # Missing arguments
-    print("Missing arguments.")
-elif str(sys.argv[1]) == "Open":
-    # Door Open request
-    publish.single("Door_Request", "Open", qos=2, hostname="localhost")
-elif str(sys.argv[1]) == "Close":
-    # Door Close request
-    publish.single("Door_Request", "Close", qos=2, hostname="localhost")
+    print("Missing arguments. Usage: iot_publish_doorlock.py <Host> <Request>")
+
+if REQUEST in ("Open", "Close"):
+    # Door Open / Close request
+    publish.single("Door_Request", REQUEST, qos=2, hostname=HOST)
