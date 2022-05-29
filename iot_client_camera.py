12b67b1637684a5771086c2e7b78e5d1e426f59c
Shota Kameyama <shotakame@gmail.com>
2022-05-29 22:10:56 +0900

    apply a config file for easier setup

--- iot_client_camera.py
+++ iot_client_camera.py
@@ -4,14 +4,18 @@ import cv2
 import paho.mqtt.client as mqtt
 # Import metaplotlib to visualize an image
 from matplotlib import pyplot as plt
-import datetime
+import datetime, yaml
+with open('./config/config.yml', 'r') as file:
+    c = yaml.safe_load(file.read())
 
 dt_now = datetime.datetime.now()
 dt_now_format = dt_now.strftime('%Y-%m-%d-%H%M%S')
 
 def take_photo():
     # Connect to capture device
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(
+        c['client_camera']['vid_cap']
+    )
     # Get a frame from the capture device
     ret, frame = cap.read()
     # Save into a picture
@@ -22,19 +26,25 @@ def take_photo():
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("DoorCamera")
+    client.subscribe(
+        c['client_camera']['subscribe'][0])
 
 def on_message(client, userdata, msg):
     req = str(msg.payload)
     print(msg.topic + " " + req)
     if req == "b'Capture'":
         take_photo()
-        print("took Photo")
-        client.publish("Message2Controller", "Captured", 2)
+        client.publish(
+            c['client_camera']['publish'][0],
+            "Captured",
+            c['mqtt']['qos'])
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(
+    c['mqtt']['host'],
+    c['mqtt']['port'],
+    c['mqtt']['keep_alive'])
 client.loop_forever()
