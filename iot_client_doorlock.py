1f2489cc35e301ef9af87f1ee6a410fe323f2025
Shota Kameyama <shotakame@gmail.com>
2022-06-01 00:57:49 +0900

    added access control to iot devices, read config yaml and replace hard coded host, port, qos, topics

--- iot_client_doorlock.py
+++ iot_client_doorlock.py
@@ -1,4 +1,7 @@
 import paho.mqtt.client as mqtt
+from pyaml_env import parse_config, BaseConfig
+
+config =  BaseConfig(parse_config('./config/config.yml'))
 door_requests = {"b'Open'": "Door Opened", "b'Close'":"Door Closed"}
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -7,21 +10,32 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("Door_Request")
+    client.subscribe(
+        config.client_lock.subscribe.controller
+    )
+    print ("Subscribed to: " + config.client_lock.subscribe.controller)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     req = str(msg.payload)
     print(msg.topic + " " + req)
     if req in door_requests:
-        client.publish("Message2Controller", door_requests[req], 2)
+        client.publish(
+            config.client_lock.publish.controller,
+            door_requests[req],
+            config.mqtt.qos)
 
-client = mqtt.Client()
+client = mqtt.Client(protocol=mqtt.MQTTv311)
+client.username_pw_set(
+    config.client_lock.user,
+    config.client_lock.password)
 client.on_connect = on_connect
 client.on_message = on_message
 
-# client.connect("mqtt.eclipseprojects.io", 1883, 60)
-client.connect("localhost", 1883, 60)
+client.connect(
+    config.mqtt.host,
+    int(config.mqtt.port),
+    config.mqtt.keep_alive)
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
--- iot_publish_doorlock.py
+++ iot_publish_doorlock.py
@@ -4,17 +4,27 @@ iot_publish_doorlock.py without auth and tls
 
 import sys
 from paho.mqtt import publish
+import paho.mqtt.client as mqtt
+from pyaml_env import parse_config, BaseConfig
+config =  BaseConfig(parse_config('./config/config.yml'))
 
-HOST = ""
-REQUEST = ""
-
-if len(sys.argv) == 3:
-    HOST = str(sys.argv[1])
-    REQUEST = str(sys.argv[2])
-else:
+if len(sys.argv) == 1:
     # Missing arguments
-    print("Missing arguments. Usage: iot_publish_doorlock.py <Host> <Request>")
+    print("Missing arguments. Usage: iot_publish_doorlock.py <Request>")
 
-if REQUEST in ("Open", "Close"):
+if sys.argv[1] in ("Open", "Close"):
     # Door Open / Close request
-    publish.single("Door_Request", REQUEST, qos=2, hostname=HOST)
+    print (sys.argv[1])
+    publish.single(
+        config.controller.publish.lock,
+        payload=sys.argv[1],
+        qos=config.mqtt.qos,
+        hostname=config.mqtt.host,
+        port=int(config.mqtt.port),
+        protocol=mqtt.MQTTv311,
+        auth={
+            'username': config.controller.user,
+            'password': config.controller.password
+        })
+    print("published to: " + config.controller.publish.lock)
+    print("payload: " + sys.argv[1])
